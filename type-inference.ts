ca860287535b9d7f1e38bbe717de87dd9419dbc8
Lucas <lxavier.00@outlook.com>
2021-02-28 13:19:06 -0300

    Aprendido sobre Types inference

---
+++ type-inference.ts
@@ -0,0 +1,11 @@
+//Type Inference
+//Ele identifica o tipo da variavel, e tambem não pode ser mudado o tipo
+//somente o conteudo
+let text = 'title'
+text = 'new title'
+
+
+window.addEventListener('click', (e) => {
+    console.log(e.target);
+    // console.log(e.foo);
+})
